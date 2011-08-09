d17fdb088f6091740ad0152f051ddefcecb8bc44
Roman Gonzalez <romanandreg@gmail.com>
2011-08-09 00:26:04 +0000

    Updating the way the vim module works

--- vim/manifests/install.pp
+++ vim/manifests/install.pp
@@ -1,51 +1,52 @@
 class vim::install {
+  include apt
 
   case $operatingsystem {
 
     "Ubuntu": {
       if versioncmp($operatingsystemrelease, '11.04') < 0 {
+        # before natty, we had vim7.2 on the sources
+        # we are going to modify the sources and do some 
+        # pinning on the vim packages we need.
 
-        file { "/tmp/vim_install":
-          ensure => directory,
-        }
-
-        file { "/tmp/vim_install/vim73.deb":
-          ensure => present,
-          source => "puppet:///modules/vim/packages/vim_7.3.035+hg~8fdc12103333-1ubuntu7_i386.deb",
-          require => File["/tmp/vim_install/vim_common.deb"],
-        }
-
-        file { "/tmp/vim_install/vim_common.deb":
-          ensure => present,
-          source => "puppet:///modules/vim/packages/vim-common_7.3.035+hg~8fdc12103333-1ubuntu7_i386.deb",
-          require => File["/tmp/vim_install/vim_runtime.deb"],
-        }
-
-        file { "/tmp/vim_install/vim_runtime.deb":
-          ensure => present,
-          source => "puppet:///modules/vim/packages/vim-runtime_7.3.035+hg~8fdc12103333-1ubuntu7_all.deb",
-          require => File["/tmp/vim_install"],
-        }
-        
-        package { "vim":
-          provider => dpkg,
-          ensure => latest,
-          source => ["/tmp/vim_install/vim_common.deb",
-                     "/tmp/vim_install/vim_runtime.deb",
-                     "/tmp/vim_install/vim73.deb"],
-          require => File["/tmp/vim_install/vim73.deb"],
-        }
-
-      }
-      else {
-
-        package { "vim":
+        # We remove conflicting packages first
+        package { ["ubuntu-minimal", "vim-tiny", "vim-common"]:
           ensure => purged,
         }
 
+        # We add the natty source
+        apt::source { 
+          "deb http://us.archive.ubuntu.com/ubuntu/ natty main restricted":
+        }
+
+        # We put a pinning for all packages natty and
+        # set a value of -100 so that they are not the 
+        # default ones
+        apt::release_pinning { "natty": }
+
+        # We do pinning of the packages we need, in this case
+        # the vim ones
+        apt::pinning { "vim": 
+          release  => "natty",
+          priority => "900",
+        }
+        apt::pinning { "vim-runtime":
+          release => "natty",
+          priority => "900",
+        }
+        apt::pinning { "vim-common":
+          release => "natty",
+          priority => "900",
+        }
+
+
+      } # End Older than natty
+
+      package { "vim":
+        ensure => latest,
       }
 
-    }
+    } # End Ubuntu
 
     default: { err("vim supported only in ubuntu") }
   }
