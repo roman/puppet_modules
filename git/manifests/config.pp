659ffc36c7ac4fc8ed3f4230a79c634fac6d15c3
Roman Gonzalez <romanandreg@gmail.com>
2011-08-10 05:28:48 +0000

    Improving git modules

--- git/manifests/config.pp
+++ git/manifests/config.pp
@@ -2,17 +2,22 @@ class git::config {
   $serial = "2011_08_06"
   $serialfile = "/var/log/puppet/gitconfig.serial"
 
-  exec { "Setup git globals":
-    require => Class["git::install"],
+  file { "/var/log/puppet": 
+    ensure => directory,
+  }
+
+  exec { "setup git globals":
     path    => ["/usr/bin", "/usr/local/bin"],
-    unless => "test \"`cat $serialfile 2> /dev/null`\" = \"$serial\"",
-    logoutput => true,
     command => "git config --system alias.st status \
 && git config --system alias.co checkout \
 && git config --system alias.ci commit \
 && git config --system alias.cp cherry-pick \
 && git config --system color.ui auto \
 && echo \"$serial\" > $serialfile",
+    creates => $serialfile,
+    logoutput => true,
+    unless => "test \"`cat $serialfile 2> /dev/null`\" = \"$serial\"",
+    require => [Class["git::install"], File["/var/log/puppet"]],
   }
 
 }
