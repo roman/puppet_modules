981e7c9eba37479b6981bf9d2d8378e272c5acb9
Roman Gonzalez <romanandreg@gmail.com>
2012-08-01 18:45:27 -0700

    Updating the git::config type

--- git/manifests/config.pp
+++ git/manifests/config.pp
@@ -2,14 +2,14 @@ define git::config($user, $value) {
   include git::install
 
   $section   = regsubst($name, "(\w+)\.(\w+)", "\[\1\]")
-  $attribute = regsubst($name, "(\w+)\.(\w+)", "\2 = $value")
+  $attribute = regsubst($name, "(\w+)\.(\w+)", "\2 = ${value}")
 
-  exec { "git-config-$name":
+  exec { "git::config/git-config-${name}":
     path => ["/bin", "/usr/bin"],
     cwd => "/home/$user/",
     user => $user,
     group => $user,
-    command => "git config --file /home/$user/.gitconfig $name $value",
+    command => "git config --file /home/$user/.gitconfig ${name} ${value}",
     unless  => "cat /home/$user/.gitconfig | grep -A5 \"$section\" | grep \"$attribute\"",
     require => Class["git::install"],
   }
