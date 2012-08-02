f24e7219bae480cf6a5f07b588c5b84c6c815b3f
Roman Gonzalez <romanandreg@gmail.com>
2012-08-01 18:10:44 -0700

    Adding support for oracle version on ubuntu using apt::ppa

--- java/manifests/install.pp
+++ java/manifests/install.pp
@@ -1,20 +1,38 @@
-class java::install {
+class java::install($java_version="oracle") {
 
   case $operatingsystem {
 
     "Ubuntu": {
       include apt
-      package { ["openjdk-6-jdk", "default-jdk"]:
-        ensure => present,
-        require => Class["apt::update"],
+
+      if $java_version == "oracle" {
+        apt::ppa { "java_ppa":
+          ppa_name => "ppa:webupd8team/java"
+        }
+        package { ["oracle-jdk7-installer"]:
+          ensure => present,
+          require => [Apt::Ppa["java_ppa"],
+                      Class["apt::update"]]
+        }
+      }
+      else {
+        package { ["openjdk-6-jdk", "default-jdk"]:
+          ensure => present,
+          require => Class["apt::update"],
+        }
       }
     }
 
     "Archlinux": {
       include pacman
-      package { "openjdk6":
-        ensure => present,
-        require => Class["pacman::config"],
+      if $java_version == "oracle" {
+        err("java oracle module not supported for $operatingsystem")
+      }
+      else {
+        package { "openjdk6":
+          ensure => present,
+          require => Class["pacman::config"],
+        }
       }
     }
 
